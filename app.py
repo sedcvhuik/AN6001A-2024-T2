d41224b213a51e81d4c9532e1658259fc6a7345c
sedcvhuik <sedcvhuik@gmail.com>
2024-11-18 08:47:01 +0000

    create main page

--- app.py
+++ app.py
@@ -5,9 +5,14 @@ from flask import Flask,request,render_template
 app = Flask(__name__)
 
 @app.route("/",methods = ["GET","POST"])
-
-def home():
+def index():
     return render_template("index.html")
+
+@app.route("/main", methods=["GET", "POST"])
+def main():
+    name = request.form.get("q")
+    return render_template("main.html")
+
 
 if __name__ == "__main__":
      app.run()
