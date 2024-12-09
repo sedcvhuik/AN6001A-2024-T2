8cae1a603141a7796d43fba3f42edadc3e4fb994
sedcvhuik <sedcvhuik@gmail.com>
2024-12-09 08:34:16 +0000

    try update genai

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 import google.generativeai as genai
 
 api = os.getenv("makersuite")
-genai.configure(api_key=api)
+genai.configure(api_key= api)
 model = genai.GenerativeModel("gemini-1.5-flash")
 
 
